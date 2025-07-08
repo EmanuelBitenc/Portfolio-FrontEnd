7ad1c8cf71d1f3967b7a7c60a3932e526c7a6a59
Emanuel Bitencourt <emanuelmagalhaes3m@gmail.com>
2025-07-08 00:25:57 -0300

    feat: correção

---
+++ src/components/Header-Panel/index.tsx
@@ -0,0 +1,9 @@
+const HeaderPanel = () => {
+  return (
+    <section className="grid grid-cols-2 gap-5">
+      <div></div>
+      <div></div>
+    </section>
+  );
+};
+export default HeaderPanel;
