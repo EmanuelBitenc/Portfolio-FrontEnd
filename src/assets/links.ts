9f8a3496fc56f08a2b267eb090e35be39f291e26
Emanuel Bitencourt Magalhães <63247451+EmanuelBitenc@users.noreply.github.com>
2025-10-29 19:37:37 -0300

    Update links.ts

--- src/assets/links.ts
+++ src/assets/links.ts
@@ -1,4 +1,5 @@
 export const LinkedinPerfil = "https://www.linkedin.com/in/emanuelbitencourt/";
 export const GithubPerfil = "https://github.com/EmanuelBitenc";
 export const curriculo =
-  "https://docs.google.com/document/d/1juxWRbq6h5EGm5wc4KGPFoHmR9aQZ1bLyy75T5NCVTE/edit?usp=sharing";
+  "https://docs.google.com/document/d/1GgPfkkT9j0XSTbvF9LMrEsVwXEU6cYBG0gYVHM9bDDQ/edit?usp=sharing";
+
