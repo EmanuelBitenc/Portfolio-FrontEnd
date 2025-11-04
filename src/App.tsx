75055c2b89150b7f0b3d17865f21f60c8e6a888d
Emanuel Bitencourt <emanuelmagalhaes3m@gmail.com>
2025-11-04 10:12:28 -0300

    add vercel/analytics

--- src/App.tsx
+++ src/App.tsx
@@ -5,6 +5,7 @@ import HeaderPanel from "./components/Header-Panel";
 import Skills from "./components/Skills";
 import Projects from "./components/Projects";
 import Footer from "./components/Footer";
+import { Analytics } from "@vercel/analytics/react";
 
 function App() {
   return (
@@ -19,6 +20,7 @@ function App() {
         <Projects />
         <Footer />
       </div>
+      <Analytics />
     </>
   );
 }
